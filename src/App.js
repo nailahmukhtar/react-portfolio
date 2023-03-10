a9f252ce28c1e82066f160477495dd96a738cb63
Nai <nailahmukhtar@Nailahs-Air.home>
2023-03-10 19:43:34 +0000

    created basic component pages and setup react-routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'materialize-css/dist/css/materialize.min.css';
+import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
