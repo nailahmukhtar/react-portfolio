146ec8d0a48db17c5c1fbb4f9964b0a945ef5cf2
Nai <nailahmukhtar@Nailahs-Air.home>
2023-03-12 17:30:20 +0000

    added a json file for my projects and setup props in the project card component

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,40 +1,21 @@
 import React from 'react'
 
-function ProjectCard() {
+function ProjectCard(props) {
   return (
-    
   <div class="row">
         <div class="col s12 m6 l4">
             <div class="card medium">
-            <div class="card-image">
-                <img src="https://placehold.co/100x100"/>
-                <span class="card-title">Card Title</span>
-            </div>
-            <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-            </div>
-            </div>
-        </div>
-        <div class="col s12 m6 l4">
-            <div class="card medium">
-            <div class="card-image">
-                <img src="https://placehold.co/100x100"/>
-                <span class="card-title">Card Title</span>
-            </div>
-            <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-            </div>
-            </div>
-        </div>
-        <div class="col s12 m6 l4">
-            <div class="card medium">
-            <div class="card-image">
-                <img src="https://placehold.co/100x100"/>
-                <span class="card-title">Card Title</span>
-            </div>
-            <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-            </div>
+                <div class="card-image">
+                    <img src={props.image}/>
+                    <span class="card-title">{props.name}</span>
+                </div>
+                <div class="card-content">
+                    <p>{props.description}</p>
+                </div>
+                <div class="card-action">
+                    <a href={props.github_url}>Github</a>
+                    <a href={props.deployed_url}>Application</a>
+                </div>
             </div>
         </div>
     </div>
