36e1b907c32da61e5ca73c5b55fbced40833e191
Nai <nailahmukhtar@Nailahs-Air.home>
2023-03-17 19:45:54 +0000

    added google font, and brand text

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 
+const styles = {
+  img: {
+    margin: "100px"
+  },
+  herotext: {
+    margin: "250px 0px 0px 0px"
+  },
+  h1: {
+    fontFamily: "Roboto, sans-serif",
+    margin: "0px"
+  }
+}
+
 function Home() {
   return (
     <div className='container'>
-      <div class="row container">
+      <div className="row">
         <div class="col s6">
-          <img src={require=('/public/Avatar.png')}/>
+          <img style={styles.img} src={require=('/Avatar.png')}/>
         </div>
-        <div class="col s6">
-          TEXT WILL GO HERE.
+        <div style={styles.herotext} className="col s6">
+          <h1 style={styles.h1}>Hello, I'm Nailah</h1>
+          <h1 style={styles.h1}>A React Developer</h1>
+          <h1 style={styles.h1}>Based in London.</h1>
         </div>
       </div>
     </div>
