1a93709065c0c8efce2206295edfd1cbe35d2920
Nai <nailahmukhtar@Nailahs-Air.home>
2023-03-19 19:58:36 +0000

    adjusted all image urls to remove proceeding /

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Header />
         <div>
           <Routes>
-            <Route path="/react-portfolio/" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="projects" element={<Projects />} />
             <Route path="contact" element={<Contact />} />
           </Routes>
