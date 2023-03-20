c2c9c49557d0fa984feb50db1bf4a7d1f16ba1fc
Nai <nailahmukhtar@Nailahs-Air.home>
2023-03-20 21:13:26 +0000

    added basename back in

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 
 function App() {
     return (
-      <Router>
+      <Router basename="/react-portfolio">
         <Header />
         <div>
           <Routes>
